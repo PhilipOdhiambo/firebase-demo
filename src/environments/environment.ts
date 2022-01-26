3a68c5682de9b7399618e3ad585b9209a54b1653
PhilipOdhiambo <jofonowino@gmail.com>
2022-01-26 13:52:14 +0300

    add environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,14 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  *******
+  apiKey: "AIzaSyDZ9iukCzcXXUFgQvcOtdZ645jRtRbCJY4",
+  authDomain: "fir-demo-b08d7.firebaseapp.com",
+  databaseURL: "https://fir-demo-b08d7.firebaseio.com",
+  projectId: "fir-demo-b08d7",
+  storageBucket: "fir-demo-b08d7.appspot.com",
+  messagingSenderId: "448168119738",
+  appId: "1:448168119738:web:b49d753484c2da92876183",
+  measurementId: "G-YY16C9JLE0"
 };
 
 /*
